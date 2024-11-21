6de1bc7ecfbd1984e3c7791196c50ba08fb372ba
Simo <simeonov.pa@gmail.com>
2024-11-21 22:12:28 +0000

    Added catView routing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import page from "./lib/page.js";
+import catsView from "./views/catsView.js";
 import homeView from "./views/homeView.js";
 
 
 // Setup routes
 page('/', homeView);
+page('/cats', catsView);
 
 // Start routing
 page();
